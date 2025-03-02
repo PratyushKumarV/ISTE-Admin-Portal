5d9967ddb4c5ad5828777ef20b6614e8d97c8e30
Pratyush <pratyushkumarv@gmail.com>
2025-03-02 12:19:18 +0530

    added necessary files

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,25 @@
 const express=require("express")
-const app=express()
+const dotenv=require("dotenv")
 
 const projects=require("./routes/projects")
 const blogs=require("./routes/blogs")
+const events=require("./routes/events")
+const resources=require("./routes/resources")
+const gallery=require("./routes/gallery")
+
+const connectDB=require("./config/db")
+
+dotenv.config()
+const app=express()
+
+const PORT=process.env.PORT || 3000
 
 app.use("/api/projects", projects)
 app.use("/api/blogs", blogs)
+app.use("/api/events", events)
+app.use("/api/resources", resources)
+app.use("/api/gallery", gallery)
 
-app.listen(5000, ()=>{
-    console.log("Server listening on port 5000...")
+app.listen(PORT, ()=>{
+    console.log(`Server listening on port ${PORT}...`)
 })
